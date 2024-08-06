e693b4ca7f90b70a7800aff94174379792b6ee52
SergeiKrk <krjukov_s@mail.ru>
2024-08-06 11:02:24 +0400

    установил @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 
 import sitemap from "@astrojs/sitemap";
@@ -12,8 +11,8 @@ export default defineConfig({
   site: "https://astro-theme-one.vercel.app/",
   integrations: [
     tailwind(),
-    image(),
     icon(),
+    image(),
     sitemap(),
     partytown({
       config: {
