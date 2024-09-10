93caf5027d3eb77234707afc2d2f772554c922fa
SergeiKrk <krjukov_s@mail.ru>
2024-09-10 16:16:45 +0400

    добавил подходящие icons в featureData

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-theme-one.vercel.app/",
+  site: "https://greencrown.ru/",
   image: {
     service: {
       entrypoint: "astro/assets/services/noop",
